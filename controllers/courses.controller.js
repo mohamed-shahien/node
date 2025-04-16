f0b638aa36c6dbd6b425c0593e67365e4f0d39ec
mohamed-shahien <shaheenagain@gmail.com>
2025-04-16 17:17:00 +0200

    add jsformatter

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,45 +1,53 @@
 
 const { validationResult } = require('express-validator');
 const course = require('../models/course.model')
+const { SUCCESS, FAIL, ERROR } = require('../utils/httpsStatus');
 
 const getCours = async (req, res) => {
-        const courses = await course.find()
-        res.json(courses)
+        let Qery = req.query;
+        const limet = Qery.limit || 4;
+        const page = Qery.page || 1;
+        const skip = (page - 1) * limet;
+        try {
+                const courses = await course.find({}, {"__v": false}).limit(limet).skip(skip)
+                return res.status(200).json({ status: SUCCESS, data: { courses } })
+        } catch (arror) {
+                return res.status(400).json({ status: ERROR, Code: 400, data: null, message: arror.message })
+        }
+
 }
 const gatSingleCourse = async (req, res) => {
         try {
-
                 const cours = await course.findById(req.params.idcours)
                 if (!cours) {
-                        return res.status(404).json({ msg: "cours not found" })
+                        return res.status(404).json({ status: FAIL, data: { title: "thes course not found" } })
                 }
-                return res.status(200).json(cours)
+                return res.status(200).json({ status: SUCCESS, data: { cours } })
         } catch (err) {
-                return res.status(400).json({msg: "invaled object id m"})
+                return res.status(400).json({ status: ERROR, message: err.message, Code: 400, data: null })
         }
 }
 const addCourse = async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-                return res.status(400).json(errors.array())
+                return res.status(400).json({ status: ERROR, Code: 400, data: errors.array() })
         }
-        const newCourse = new course(req.body)
+        const newCourse = new course(req.body) 
         await newCourse.save()
-        res.status(200).json(newCourse)
+        res.status(200).json({ status: SUCCESS, data: { newCourse } })
 }
 const updataCourse = async (req, res) => {
         const coursID = req.params.idcours;
         try {
-                const updataCourse = await course.updateOne({_id: coursID}, {$set : {...req.body}})
-                res.status(200).json(updataCourse)
+                const updataCourse = await course.updateOne({ _id: coursID }, { $set: { ...req.body } })
+                res.status(200).json({ status: SUCCESS, data: { updataCourse } })
         } catch (err) {
-                return res.status(400).json({msg: "can not addding"})
+                return res.status(400).json({ status: ERROR, message: err.message, Code: 400, data: null })
         }
 }
 const deleteCourse = async (req, res) => {
-        const data =  await course.deleteOne({_id: req.params.idcours}, )
-
-        res.status(200).json({ msg: "cours deleted successfully" , data});
+        const data = await course.deleteOne({ _id: req.params.idcours },)
+        res.status(200).json({ status: SUCCESS, data: { data } });
 }
 
 
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
+require("dotenv").config()
 const express = require('express');
 const app = express();
 const mongose = require('mongoose');
-const url = "mongodb+srv://mohammedmjj2025:123@cluster-test.7mz8idu.mongodb.net/codeZone?retryWrites=true&w=majority&appName=Cluster-test"
+const url = process.env.MONGO_URL
 mongose.connect(url).then(()=> {
         console.log("mongo conecetd")
 })
 app.use(express.json());
 const coursesRouter = require('./routes/courses.route');
-
 app.use('/api/courses', coursesRouter);
 
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
         console.log('listen in port 5000')
 })  
