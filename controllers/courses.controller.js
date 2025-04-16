87ca7880c823042cf3171bcd3a71e73d13c5f419
mohamed-shahien <shaheenagain@gmail.com>
2025-04-16 12:59:30 +0200

    add mongose

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -1,45 +1,45 @@
 
-let { courses } = require('../data/courses');
 const { validationResult } = require('express-validator');
-const getCours = (req, res) => {
+const course = require('../models/course.model')
+
+const getCours = async (req, res) => {
+        const courses = await course.find()
         res.json(courses)
 }
-const gatSingleCourse = (req, res) => {
-        const coursID = +req.params.idcours;
-        const cours = courses.find((cours) => cours.id === coursID)
-        if (!cours) {
-                return res.status(404).json({ msg: "cours not found" })
-        }
-        res.status(200).json(cours)
-}
-const addCourse =  (req, res) => {
-                const errors = validationResult(req);
-                if (!errors.isEmpty()) {
-                        return res.status(400).json(errors.array())
-                }
-                const cours = { id: courses.length + 1, ...req.body };
-                courses.push(cours)
-                console.log(req.body)
-                res.status(201).json(cours)
-        }
-const updataCourse = (req, res) => {
-        const coursID = +req.params.idcours;
-        let cours = courses.find((cours) => cours.id === coursID)
-        if (!cours) {
-                return res.status(404).json({ msg: "course not found" })
-        }
-        cours = { ...cours, ...req.body }
-        res.status(200).json(cours)
-}
-const deleteCourse = (req, res) => {
-        const coursID = +req.params.idcours;
-        const hasincourss = courses.find((cours) => cours.id === coursID);
-        if (!hasincourss) {
-                return res.status(404).json({ msg: "cours not found" });
-        }
-        courses = courses.filter((curs) => curs.id !== coursID);
+const gatSingleCourse = async (req, res) => {
+        try {
 
-        res.status(200).json({ msg: "cours deleted successfully" });
+                const cours = await course.findById(req.params.idcours)
+                if (!cours) {
+                        return res.status(404).json({ msg: "cours not found" })
+                }
+                return res.status(200).json(cours)
+        } catch (err) {
+                return res.status(400).json({msg: "invaled object id m"})
+        }
+}
+const addCourse = async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+                return res.status(400).json(errors.array())
+        }
+        const newCourse = new course(req.body)
+        await newCourse.save()
+        res.status(200).json(newCourse)
+}
+const updataCourse = async (req, res) => {
+        const coursID = req.params.idcours;
+        try {
+                const updataCourse = await course.updateOne({_id: coursID}, {$set : {...req.body}})
+                res.status(200).json(updataCourse)
+        } catch (err) {
+                return res.status(400).json({msg: "can not addding"})
+        }
+}
+const deleteCourse = async (req, res) => {
+        const data =  await course.deleteOne({_id: req.params.idcours}, )
+
+        res.status(200).json({ msg: "cours deleted successfully" , data});
 }
 
 
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
+const mongose = require('mongoose');
+const url = "mongodb+srv://mohammedmjj2025:123@cluster-test.7mz8idu.mongodb.net/codeZone?retryWrites=true&w=majority&appName=Cluster-test"
+mongose.connect(url).then(()=> {
+        console.log("mongo conecetd")
+})
 app.use(express.json());
 const coursesRouter = require('./routes/courses.route');
 
-app.use('/api/courss', coursesRouter);
+app.use('/api/courses', coursesRouter);
 
 app.listen(5000, () => {
         console.log('listen in port 5000')
